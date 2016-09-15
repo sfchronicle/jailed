d139a671d2c82510b57dfe0fa65656c01b4c1d41
Lucio Villa <lucio310@gmail.com>
2016-09-15 13:43:48 -0700

    added social, final photo edit and resources infobox.

--- views.py
+++ views.py
@@ -8,13 +8,13 @@ from models import *
 app.config['PROJ_TITLE'] = ''
 
 # Site Path/Slug
-app.config['PATH'] = ''
+app.config['PATH'] = 'captive-lives'
 
 # Project Category for Omniture 
-app.config['CATEGORY'] = ''
+app.config['CATEGORY'] = 'Metro'
 
 # Project Hashtag
-app.config['HASHTAG'] = ''
+app.config['HASHTAG'] = 'CaptiveLives'
 
 """
 slug completes:
@@ -42,6 +42,14 @@ twitter_text:
 def index():
     return render_template('index.html',
     	slug='',
-    	title="",
-    	description="",
-    	twitter_text="")
+    	title="Children of inmates face long odds of success",
+    	description="Captive Lives: Studies show the needs of some 10 million children with incarcerated parents have been largely ignored. But slowly, efforts to help them are growing.",
+    	twitter_text="Special report: Children of incarcerated parents face long odds of success.")
+
+@app.route('/visits/')
+def visits():
+    return render_template('visits.html',
+    	slug='visits',
+    	title="SF jail helps families bond behind bars",
+    	description="Captive Lives: While many jails reduce or eliminate in-person visits, San Francisco's works to bring inmates and their children together.",
+    	twitter_text="SF jail works to help families bond behind bars.")
